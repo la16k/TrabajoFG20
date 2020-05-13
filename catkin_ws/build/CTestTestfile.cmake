ec0306c55916ed8e217ef05b3e96205cd1cb9718
Laura <kunito.laura.ac@gmail.com>
2020-05-13 18:08:13 +0200

    Tutorial
    Intento de replica de tutoriales de Gazebo

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/lak/Escritorio/TFG/TrabajoFG20/catkin_ws/src
+# Build directory: /home/lak/Escritorio/TFG/TrabajoFG20/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("description")
+subdirs("gazebo")
---
+++ catkin_ws/build/description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lak/Escritorio/TFG/TrabajoFG20/catkin_ws/src/description
+# Build directory: /home/lak/Escritorio/TFG/TrabajoFG20/catkin_ws/build/description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
