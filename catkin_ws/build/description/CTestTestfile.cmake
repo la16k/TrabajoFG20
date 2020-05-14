95a7fc79a3628f2bfe1ae62b7e291e82ad4d187b
Laura <kunito.laura.ac@gmail.com>
2020-05-14 22:22:09 +0200

    Mundo
    primer mundo completo conseguido

--- catkin_ws/build/description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lak/Escritorio/TFG/TrabajoFG20/catkin_ws/src/description
-# Build directory: /home/lak/Escritorio/TFG/TrabajoFG20/catkin_ws/build/description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
