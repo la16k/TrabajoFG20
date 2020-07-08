dd1f8595ad538c25548b1947ec911e3bf06a71fe
Laura <kunito.laura.ac@gmail.com>
2020-07-08 19:53:49 +0200

    Intento mov 3.0
    Con codigo .cc adaptado del tutorial

---
+++ simulation_ws/src/cl/MovePlugin.cc
@@ -0,0 +1,55 @@
+#include <gazebo/gazebo.hh>
+#include <sdf/sdf.hh>
+#include <ignition/math.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+
+namespace gazebo
+{
+    class Move : public ModelPlugin 
+    {
+        public: void Load(physics::ModelPtr mundo, sdf::ElementPtr _sdf)
+            {
+                this->model = mundo;
+
+                gazebo::common::PoseAnimationPtr animacion(
+                   new gazebo::common::PoseAnimation("test", 10.0, true)
+                );
+
+                gazebo::common::PoseKeyFrame *key;
+
+                key = animacion->CreateKeyFrame(0);
+                key->Translation(ignition::math::Vector3d(-0.885, 0.5, 0));
+                key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+
+                key = animacion->CreateKeyFrame(2.0);
+                key->Translation(ignition::math::Vector3d(-0.885, 0, 0));
+                key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+                
+                key = animacion->CreateKeyFrame(4.0);
+                key->Translation(ignition::math::Vector3d(-0.885, -0.5, 0));
+                key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+
+                key = animacion->CreateKeyFrame(6.0);
+                key->Translation(ignition::math::Vector3d(-0.885, 0, 0));
+                key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+
+                key = animacion->CreateKeyFrame(8.0);
+                key->Translation(ignition::math::Vector3d(-0.885, 0.5, 0));
+                key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+
+                key = animacion->CreateKeyFrame(10.0);
+                key->Translation(ignition::math::Vector3d(-0.885, 0, 0));
+                key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+
+                mundo->SetAnimation(animacion);
+            }
+
+            private: physics::ModelPtr model;
+
+            private: event::ConnectionPtr updateConection;
+    };
+
+    GZ_REGISTER_MODEL_PLUGIN(Move)
+}
